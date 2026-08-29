88dd882687744df23da902754664c706d907b109
Mohd Hamka <122353752+mhdhamka@users.noreply.github.com>
2026-08-29 22:30:04 +0800

    Change import statement for GitHubCalendar

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useMemo, useRef } from 'react';
 import Image from 'next/image';
-import GitHubCalendar from 'react-github-calendar';
+import { GitHubCalendar } from 'react-github-calendar';
 import { 
   VscFolderOpened, 
   VscGithub, 
